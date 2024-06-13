fa006c808a46f8359f59ff595367678d7171e290
Ronak <ronakfeedback@gmail.com>
2024-06-13 19:29:21 +0100

    GOKU BLACK

--- main-menu.js
+++ main-menu.js
@@ -7,3 +7,7 @@ document.getElementById('pixipal1').addEventListener('click', () => {
 document.getElementById('pixipal2').addEventListener('click', () => {
     ipcRenderer.send('launch-pixipal', 'Noelle');
 });
+
+document.getElementById('pixipal3').addEventListener('click', () => {
+    ipcRenderer.send('launch-pixipal', 'Goku_Black');
+});
